43e7e25814df18b6e282cbc3b016c2ce73aa1f89
Scott-James-Hurley <n1nt3nd01@gmail.com>
2019-02-03 02:29:42 +0000

    stub

--- src/Astro-Core/spice_general_functions.cpp
+++ src/Astro-Core/spice_general_functions.cpp
@@ -1,3 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*
+-------Copyright (C) 2018 University of Strathclyde and Authors-------
+--------- Author: Scott Hurley (GitHub: Scott_James_Hurley)-----------
+-------- e-mail: scott.james.hurley.97@gmail.com ---------------------
+*/
+
 #include "Astro-Core/spice_general_functions.h"
 
 using namespace smartastro;
